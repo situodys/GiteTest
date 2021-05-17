c6989a8d1bc3ae1a1d8c10f4c45ce01e090ad0fc
leejh <situodys@gmail.com>
2021-05-17 16:31:45 +0900

    Modify main.c in testbranch

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
-#include "myheader.h"
+#include <stdio.h
+>
+#include "myheader.h
+"
 int main(void) {
+char name[17] = {0,};
+printf("Name: ");
+scanf("%s", name);
 printHello();
 printBye();
 return 0;
-}
+}}
